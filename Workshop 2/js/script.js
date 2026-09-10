0d20efefd062a74227cbfbe219c78293f947b1b3
Millimalau <milli.haverinen@student.laurea.fi>
2026-09-10 14:40:23 +0300

    Uusi palautus

---
+++ Workshop 2/js/script.js
@@ -0,0 +1,47 @@
+// STEP 1: Print text to the browser console is string ""
+console.log("Hello, World!");
+
+
+// STEP 2: Variables
+
+// Store text values in variables
+
+const userName = "Milli";
+const lempiEläin = "Kissa";
+
+
+// Print the variables to the console
+console.log("Minun nimi on " + userName);
+console.log("Lempi eläimeni on " + lempiEläin);
+
+// STEP 3: User interaction
+
+
+// Display a pop-up message
+
+alert("Tervetuloa sivuilleni!");
+
+// Ask the user for their name
+
+const visitorName = prompt("Mikä on nimesi?");
+
+// Print the user's answer
+
+console.log("Terve " + visitorName + "! Tervetuloa javascrptin pariin!");
+
+// Ask the user for their favorite animal
+
+const visitorFavoriteAnimal = prompt("Mikä on lempi eläimesi?");
+
+// Create a sentence using both answers
+
+console.log("Terve " + visitorName + "! Sinun lempi eläimesi on " + visitorFavoriteAnimal + ".");
+
+// console.log("Hello, " + visitorName + "! Your favorite animal is " + visitorFavoriteAnimal + ".");
+
+// ⭐⭐ BONUS Ask the user for their favorite animal.If their favorite animal is "Blue Elephant", print: Great choice! That's my favorite animal too!
+// ⭐⭐ BONUS Otherwise, print: Nice! Your favorite animal is [animal]. Can you figure out how to do this using if / else?
+
+
+
+// const visitorFavoriteAnimal = prompt("What is your favorite animal?");
